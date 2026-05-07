c4289f1e370012512c88f196781b4a4c21896b35
papersplx <a9pbg0vj6@mozmail.com>
2026-05-07 16:59:41 +0200

    Fix search box with relative wrapper

--- spooky2-search/src/components/SearchBox.jsx
+++ spooky2-search/src/components/SearchBox.jsx
@@ -24,7 +24,7 @@ function SearchBox({ query, onSearch }) {
   }, [value, onSearch]);
 
   return (
-    <div className="search-box">
+    <div className="search-box-wrapper">
       <input
         type="text"
         className="search-input"
@@ -38,7 +38,11 @@ function SearchBox({ query, onSearch }) {
           className="clear-btn"
           onClick={() => setValue('')}
           aria-label="Clear search"
-        />
+        >
+          <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M7 5.67L2.67 1.34L1.34 2.67L5.67 7L1.34 11.33L2.67 12.66L7 8.33L11.33 12.66L12.66 11.33L8.33 7L12.66 2.67L11.33 1.34L7 5.67Z" fill="currentColor"/>
+          </svg>
+        </button>
       )}
     </div>
   );
